573cc83607f3afa64fddb3b1075bd2ad15cbd1a4
nodbox <nodbox.company@gmail.com>
2022-04-17 19:41:46 +0530

    added port variable

--- nodbox.py
+++ nodbox.py
@@ -4,19 +4,20 @@ import time
 from threading import Thread
 import webview
 
+port=8889
 
 def start_webview():
-    window = webview.create_window('Nodbox', 'http://localhost:8000/', confirm_close=True, width=900, height=600)
+    window = webview.create_window('Nodbox', f'http://localhost:{port}/', confirm_close=True, width=900, height=600)
     webview.start()
     window.closed = os._exit(0)
 
 
 def startApp():
     if sys.platform in ['win32', 'win64']:
-        os.system("python manage.py runserver {}:{}".format('127.0.0.1', '8000'))
+        os.system("python manage.py runserver {}:{}".format('127.0.0.1', port))
         
     else:
-        os.system("python3 manage.py runserver {}:{}".format('127.0.0.1', '8000'))
+        os.system("python3 manage.py runserver {}:{}".format('127.0.0.1', port))
        
 
 
